e1d8a8b786063061ebaf7fb09db8d981f134e8dd
tuleshghelani <ghelanitulesh@gmail.com>
2022-10-08 21:34:46 +0530

    Added host

--- Online_Auction/settings.py
+++ Online_Auction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_igtoud8=l*xejhiwtfz$ce-fu*%80&^hybem91#g#)5j7td#z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
